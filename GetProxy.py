cca5ebf25faa0c672e0388cf72c45757bf9e89d7
8023 <lxy_05@126.com>
2016-07-27 17:42:22 +0800

    add Data.db, write GetProxy.py

--- GetProxy.py
+++ GetProxy.py
@@ -5,39 +5,30 @@
 # @Last Modified by:   8023
 
 import requests
+import json
 
 class GetProxy(object):
     """docstring for GetProxy"""
     def __init__(self, arg):
-        self.all = 1
-
-        self.anonymous = 2
-        self.transparent = 3
-
-        self.cn = 2
-        self.intl = 3
-
-        self.ca = {'kuai' : }
-        self.ct = {'kuai' : }
-        self.ia = {'kuai' : }
-        self.it = {'kuai' : }
-
-        self.visibility = {'kuai' : 'http://www.kuaidaili.com/free/',
-                           'xici' : 'http://www.xicidaili.com/'}
+        self.all = 'all'
+        self.ca = 'cn_anonymous'
+        self.ct = 'cn_transparent'
+        self.ia = 'intl_anonymous'
+        self.it = 'intl_transparent'
 
         # super(GetProxy, self).__init__()
         # self.arg = arg
 
-    def get_html(self, providers = self.all, country = self.all, visibility = self.all):
-        pass
+    # def get_html(self, providers = self.all, country = self.all, visibility = self.all):
+    #     pass
 
-    def fetch_url():
-        pass
-
-    def
+    # def fetch_url(self):
+    #     pass
 
 
 if __name__ == '__main__':
-    r = requests.get('http://www.baidu.com')
-    print(r.text)
-    # print('dui')
+    # r = requests.get('http://www.baidu.com')
+    # print(r.text)object
+
+    with open('Data.db', 'r') as f:
+        data = json.load(f)
