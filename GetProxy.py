3647cc988c27d946c268e2e9287f3284a457da98
8023 <lxy_05@126.com>
2016-07-28 18:40:09 +0800

    add get_url, textOK, add xici maxpage regular to Data.json

--- GetProxy.py
+++ GetProxy.py
@@ -12,12 +12,12 @@ class GetProxy(object):
     """docstring for GetProxy"""
     def __init__(self):
         #built-in regular
-        self.scheme = r'(?P<scheme>https?|HTTPS?)'
-        self.host = r'(?P<host>(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3})'
-        self.port = r'(?P<port>\d{1,5})'
-        self.maxpage = r'(?P<maxpage>\d{1,6})'
+        self.SCHEME = r'(?P<scheme>https?|HTTPS?)'
+        self.HOST = r'(?P<host>(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)){3})'
+        self.PORT = r'(?P<port>\d{1,5})'
+        self.MAXPAGE = r'(?P<maxpage>\d{1,6})'
         #requests headers
-        self.headers = {
+        self.HEADERS = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
         }
 
@@ -25,45 +25,19 @@ class GetProxy(object):
         with open('Data.json', 'r') as f:
             self.data = json.load(f)
 
-    # def get_proxies(self, num, provider, pattern):
-    #     l = []
-    #     for x in get_html(provider, pattern):
-    #         for y in get_url(x, provider):
-    #             l += y
-    #             if len(l) >= num:
-    #                 return l
-
-    def get_url(self, provider, pattern, html):
-        for x in re.finditer(r[provider]['re'].format(scheme = self.scheme, host = self.host, port = self.port), html, re.S):
-            print(x.group('scheme').lower() + '://' + x.group('host') + ':' + x.group('port'))
+    def get_url(self, provider, html):
+        l = []
+        for x in re.finditer(self.data[provider]['re'].format(scheme = self.SCHEME, host = self.HOST, port = self.PORT), html, re.S):
+            l.append({x.group('scheme').lower(): x.group('scheme').lower() + '://' + x.group('host') + ':' + x.group('port')})
+        return (l)
 
     def get_html(self, provider, pattern, page):
-        return requests.get(self.data[provider][pattern].format(page), headers = self.headers).text
+        return requests.get(self.data[provider][pattern].format(page = page), headers = self.HEADERS).text
 
     def get_maxpage(self, provider, html):
-        return int(re.search(self.data[provider]['maxpage'].format(maxpage = self.maxpage), html, re.S).group('maxpage'))
+        return int(re.search(self.data[provider]['maxpage'].format(maxpage = self.MAXPAGE), html, re.S).group('maxpage'))
 
 if __name__ == '__main__':
     proxies = GetProxy()
-    print(proxies.get_maxpage('kuai', proxies.get_html('kuai', 'cn_transparent', 1)))
-
-    # print(html.text)
-
-    # proxies = GetProxy();
-    # proxies.get_url(pattern = proxies.ca);
-
-
-        # print(v)
-        # v[cn_anonymous]
-    # for k in proxies.data:
-
-    #     print(k)
-
-    # print(proxies.data)
-
-
-
-    # 获取最大页面()
-    # 循环获取html代码
-    # 正则匹配()
-    # 输出list
+    print(proxies.get_maxpage('xici', proxies.get_html('xici', 'cn_transparent', 1)))
+    # print(proxies.get_url('kuai', proxies.get_html('kuai', 'cn_transparent', 1)))
